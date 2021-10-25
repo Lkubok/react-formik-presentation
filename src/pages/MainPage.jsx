776d587018358a254e81689aaf6dfe1ee1d2802f
Lucas <lkubok@gmail.com>
2021-10-25 23:34:07 +0200

    created raw form withput any business logic

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,56 @@
 import React from "react";
 import "../styles/MainPage.css";
 
+import { Form, Button } from "react-bootstrap";
+
 export default function MainPage() {
   return (
     <div className="container">
-      <div>centered text</div>
+      <Form className="form">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>User name</Form.Label>
+          <Form.Control type="text" placeholder="User name" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Second name</Form.Label>
+          <Form.Control type="text" placeholder="Second name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control type="text" placeholder="Email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Repeat your password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Short info about you</Form.Label>
+          <Form.Control as="textarea" placeholder="Short info about you" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            type="checkbox"
+            label="Just accept - we will give your kidney to a homeless bum"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
